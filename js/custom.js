002df23631ec943ad7844ff537010f17c7229a0c
Yaroslav Ersteniuk <erstslavik@gmail.copm>
2020-11-08 02:09:52 +0200

    fixed navbar background; sigh-up draft

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,22 @@
 $(function () {
+    //we have 2-5 pages with white background
+    const maxWhiteIndex = 5;
     $(window).scroll(function () {
-        if ($(this).scrollTop() > $(window).height()) {
+        if ($(this).scrollTop() >= $(window).height() && 
+            $(this).scrollTop() < maxWhiteIndex * $(window).height()) {
             $('#navbar-logo-white').hide();
             $('#navbar-logo-purple').show();
+
+            $('.navbar').removeClass('bg-part-purple');
+            $('.navbar').addClass('bg-white');
         }
-        if ($(this).scrollTop() < $(window).height()) {
+        if ($(this).scrollTop() < $(window).height() ||
+            $(this).scrollTop() >= maxWhiteIndex * $(window).height()) {
             $('#navbar-logo-white').show();
             $('#navbar-logo-purple').hide();
+
+            $('.navbar').addClass('bg-part-purple');
+            $('.navbar').removeClass('bg-white');
         }
     });
 });
